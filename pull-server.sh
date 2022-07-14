c4a64c56bcd8b999d0bad7e6a8e9353187378451
Jezreel Kintino de Moraes <jezreel.aluno@gmail.com>
2022-07-14 20:48:09 -0300

    add cors and helmet

---
+++ pull-server.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+npm run build
+git add .
+
+echo "Message to commit:"
+read message
+git commit -am "$message"
+git push
+ssh server@192.168.100.40 \
+  'git -C ~/cursoJS_API_REST/' \
+  'git pull &&' \
+  'npm i &&'
+  'pm2 restart api && sudo systemctl restart nginx'
--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 import express from "express";
 import { resolve } from "path";
+import cors from "cors";
+import helmet from "helmet";
 
 import "./database";
 import homeRoutes from "./routes/homeRoutes";
@@ -18,6 +20,8 @@ class App {
   }
 
   middlewares() {
+    this.app.use(cors());
+    this.app.use(helmet());
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(express.json());
     this.app.use(
