38a4e0432fc88d0b4fc556519be136dd22c1481d
Jezreel Kintino de Moraes <jezreel.aluno@gmail.com>
2022-07-15 08:59:53 -0300

    teste

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ const whiteList = [];
 
 const corsOptions = {
   origin: (origin, callback) => {
-    if (whiteList.indexOf(origin) !== -1) {
+    if (!origin || whiteList.indexOf(origin) !== -1) {
       callback(null, true);
     } else {
       callback(new Error(`Not allowed by CORS: ${origin}`));
