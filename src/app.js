d279b32147e26f5c808380ae3f642525c5e56297
Jezreel Kintino de Moraes <jezreel.aluno@gmail.com>
2022-07-14 21:58:27 -0300

    teste

--- src/app.js
+++ src/app.js
@@ -12,17 +12,17 @@ import photoRoutes from "./routes/photoRoutes";
 
 require("dotenv").config();
 
-const whiteList = ["http://192.168.100.248:3000", "http://192.168.100.40:3001"];
+// const whiteList = ["http://192.168.100.248:3000", "http://192.168.100.40:3001"];
 
-const corsOptions = {
-  origin: (origin, callback) => {
-    if (!origin || whiteList.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-};
+// const corsOptions = {
+//   origin: (origin, callback) => {
+//     if (!origin || whiteList.indexOf(origin) !== -1) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error("Not allowed by CORS"));
+//     }
+//   },
+// };
 
 class App {
   constructor() {
@@ -32,7 +32,7 @@ class App {
   }
 
   middlewares() {
-    this.app.use(cors(corsOptions));
+    this.app.use(cors());
     this.app.use(helmet());
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(express.json());
